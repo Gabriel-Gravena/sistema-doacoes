47382c85ade4d23f8f136ed1af111c2b36749286
Gabriel Gravena <gabriel.cgravena@gmail.com>
2025-04-11 13:15:26 -0300

    first commit - add archives

---
+++ src/main/java/com/exemplo/sistemadoacoes/App.java
@@ -0,0 +1,13 @@
+package com.exemplo.sistemadoacoes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
