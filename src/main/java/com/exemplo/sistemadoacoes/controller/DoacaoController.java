eef6e45b997b83f59a06d2c0f08246a343c5336c
Gabriel Gravena <gabriel.cgravena@gmail.com>
2025-05-03 12:57:46 -0300

    feat: implementar DoacaoController: Adiciona o controlador de doações para completar a arquitetura MVC do sistema.

--- src/main/java/com/exemplo/sistemadoacoes/controller/DoacaoController.java
+++ src/main/java/com/exemplo/sistemadoacoes/controller/DoacaoController.java
@@ -1,4 +1,24 @@
 package com.exemplo.sistemadoacoes.controller;
 
+import com.exemplo.sistemadoacoes.model.Doacao;
+import com.exemplo.sistemadoacoes.model.Doador;
+import com.exemplo.sistemadoacoes.service.DoacaoService;
+import com.exemplo.sistemadoacoes.service.DoadorService;
+
+import java.util.List;
+
 public class DoacaoController {
+    private DoacaoService doacaoService;
+
+    public DoacaoController(DoadorService doadorService) {
+        this.doacaoService = new DoacaoService(doadorService);
+    }
+
+    public Doacao registrarDoacao(Doador doador, double valorDoacao) {
+        return doacaoService.registrarDoacao(doador, valorDoacao);
+    }
+
+    public double calcularTotalDoacoes(List<Doador> doadores) {
+        return doacaoService.CalcularValorTotalDoacoes(doadores);
+    }
 }
